f84f67107ae9d65be63eb42b84d9203129742579
Debby R <debbyr@Carlos-Mac.local>
2024-05-14 23:02:28 -0300

    test

---
+++ test/cadastroAnimais/src/test/java/com/cadastroAnimais/cadastroAnimais/CadastroAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastroAnimais.cadastroAnimais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
